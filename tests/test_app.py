ae4b3c87069a763351de4b425c5d08bb8160f353
tar1e <88047922+tar1e@users.noreply.github.com>
2025-08-20 13:55:22 +0000

    Add CI/CD workflow for local Docker deployment

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
-import sys, os
-
-# добавляем путь к корню проекта, чтобы импортировать app.py
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 from app import app
 
-
-def test_home():
+def test_index():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, CI/CD" in response.data
+    assert b"Новости" in response.data
